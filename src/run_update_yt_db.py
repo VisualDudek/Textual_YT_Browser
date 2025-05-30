0150d68219a3ce39aa6edc3fa4dbcd4ed9c0b55a
MarcinDudek <dudek.v@gmail.com>
2025-05-30 08:20:47 +0200

    Refactor main function in run_update_yt_db.py to encapsulate logic and improve readability

--- src/run_update_yt_db.py
+++ src/run_update_yt_db.py
@@ -3,12 +3,17 @@ from database import DatabaseService
 from config import config
 from models import YTConfig, YTChannel
 
-ytconfig = YTConfig.from_yaml(config.yt_config_file)
-dbservice = DatabaseService()
 
-for channel_data in ytconfig.channels:
+def main():
+    ytconfig = YTConfig.from_yaml(config.yt_config_file)
+    dbservice = DatabaseService()
 
-    channel = YTChannel.from_dict(channel_data)
-    videos = get_last_videos(channel, max_results=ytconfig.results)
-    dbservice.save_videos(videos)
-    pass
+    for channel_data in ytconfig.channels:
+
+        channel = YTChannel.from_dict(channel_data)
+        videos = get_last_videos(channel, max_results=ytconfig.results)
+        dbservice.save_videos(videos)
+
+
+if __name__ == "__main__":
+    main()
